a0cb50192ca90144242264fd7b4cf7a64ee3c8ad
SergioMiedes <smg163@gcloud.ua.es>
2022-06-20 18:22:02 +0200

    algunos write line

--- PracticaDSMGenNHibernate/CP/DSMPracticas/JuegoCP_recomendarJuego.cs
+++ PracticaDSMGenNHibernate/CP/DSMPracticas/JuegoCP_recomendarJuego.cs
@@ -30,7 +30,7 @@ public void RecomendarJuego (int p_usu)
             JuegoCEN juegoCEN = null;
             UsuarioCEN usuarioCEN = null;
 
-
+            Console.WriteLine("Entro a Recomendar Juego");
 
             try
             {
@@ -42,31 +42,28 @@ public void RecomendarJuego (int p_usu)
                 IList<UsuarioEN> seguidos = usuarioCEN.GetFollowed(p_usu);
 
                 List<string> juegos = new List<string>();
-                foreach (UsuarioEN seguido in seguidos)
-                { //recuperamos todos los juegos de los seguidos del usuario
+                foreach (UsuarioEN seguido in seguidos){ //recuperamos todos los juegos de los seguidos del usuario
                     IList<JuegoEN> juegosSeguido = juegoCEN.GetJuegosPorUsuario(seguido.Id);
-                    foreach (JuegoEN juego in juegosSeguido)
-                    {
+                    foreach (JuegoEN juego in juegosSeguido){
                         juegos.Add(juego.Nombre);
                     }
                 }
 
 
                 var dict = new Dictionary<string, int>(); //estas lineas cuentan cuantas veces se repiten los juegos
-                foreach (string juego in juegos)
-                {
+                foreach (string juego in juegos){
                     if (dict.ContainsKey(juego))
                         dict[juego]++;
                     else
                         dict[juego] = 1;
                 }
+                Console.WriteLine(dict);
 
                 var lista = dict.ToList();
                 lista.Sort((pair1, pair2) => pair2.Value.CompareTo(pair1.Value)); //volcamos el diccionario en una lista para ordenarlo de mas a menos veces repetido
                 int salir = 0;
                 int maxJuegosRecomendados = 5;
-                foreach (var value in lista)
-                {
+                foreach (var value in lista){
                     Console.WriteLine(value.Key);
                     if (salir >= maxJuegosRecomendados)
                         break;
