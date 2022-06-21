089626b2aa5501ba8ae53cfd790a878b92b6024f
SergioMiedes <smg163@gcloud.ua.es>
2022-06-21 20:54:49 +0200

    enviar notificacion a falta de un parametro

--- PracticaDSMGenNHibernate/CEN/DSMPracticas/NotificacionCEN_enviarNotificacion.cs
+++ PracticaDSMGenNHibernate/CEN/DSMPracticas/NotificacionCEN_enviarNotificacion.cs
@@ -11,13 +11,11 @@ using PracticaDSMGenNHibernate.EN.DSMPracticas;
 using PracticaDSMGenNHibernate.CAD.DSMPracticas;
 
 
+
 /*PROTECTED REGION ID(usingPracticaDSMGenNHibernate.CEN.DSMPracticas_Notificacion_enviarNotificacion) ENABLED START*/
 using System.Net.Mail;
-using System.Configuration;
-using System.Net.Configuration;
 using System.Net;
-using System.Security.Cryptography.X509Certificates;
-using System.Net.Security;
+
 /*PROTECTED REGION END*/
 
 namespace PracticaDSMGenNHibernate.CEN.DSMPracticas
@@ -29,21 +27,35 @@ public void EnviarNotificacion (int p_oid)
             /*PROTECTED REGION ID(PracticaDSMGenNHibernate.CEN.DSMPracticas_Notificacion_enviarNotificacion) ENABLED START*/
 
             // Write here your custom code...
-            MailMessage correo = new MailMessage();
-            correo.From = new MailAddress("gogaminggroupsl@gmail.com", "GoGaming", System.Text.Encoding.UTF8);//Correo de salida
-            correo.To.Add("smg163@gcloud.ua.es"); //Correo destino?
-            correo.Subject = "Correo de prueba"; //Asunto
-            correo.Body = "Este es un correo de prueba desde c#"; //Mensaje del correo
-            correo.IsBodyHtml = true;
-            correo.Priority = MailPriority.Normal;
-            SmtpClient smtp = new SmtpClient();
-            smtp.UseDefaultCredentials = false;
-            smtp.Host = "smtp.gmail.com"; //Host del servidor de correo
-            smtp.Port = 25; //Puerto de salida
-            smtp.Credentials = new System.Net.NetworkCredential("gogaminggroupsl@gmail.com", "ggPassword");//Cuenta de correo
-            ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
-            smtp.EnableSsl = true;//True si el servidor de correo permite ssl
-            smtp.Send(correo);
+            //Recuperamos los datos que vamos que necesitar del usuario
+            UsuarioCAD usuarioCAD = new UsuarioCAD();
+            UsuarioEN usuario = usuarioCAD.ReadOIDDefault(p_oid);
+                          
+            //Preparamos y mandamos el correo    
+            var fromAddress = new MailAddress("gogaminggroupsl@gmail.com", "Go Gaming");
+            var toAddress = new MailAddress(usuario.Email, "To Name");
+            const string fromPassword = "qamecfuphnkrpmxr";
+            const string subject = "Novedades en la comunidad de *Insertar Comunidad*";
+            const string body = "¡Hola, *Insertar nombre*! El usuario *Insertar nombre* ha subido un nuevo post a la comunidad de *Insertar Comunidad*. ¡No te lo pierdas!";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
             /*PROTECTED REGION END*/
         }
 }
