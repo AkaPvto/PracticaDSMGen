d0f3d463647cec33819953d498024d2ccb50d1b1
SergioMiedes <smg163@gcloud.ua.es>
2022-06-26 20:20:21 +0200

    Comentario avanzado; view model

--- GoGaming/Models/ComentarioViewModel.cs
+++ GoGaming/Models/ComentarioViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,18 @@ namespace GoGaming.Models
 {
     public class ComentarioViewModel
     {
-        public int Id { get; set; }
+        [ScaffoldColumn(false)]
+
+        [Display(Prompt = "Comentario", Description = "Comentario", Name = "Contenido")]
+        [Required(ErrorMessage = "Debe escribir un comentario")]
+        [StringLength(maximumLength: 400, ErrorMessage = "El comentario no puede tener más de 400 caracteres")]
+        public string Contenido { get; set; }
+
+        [Display(Prompt = "Comentario", Description = "Comentario", Name = "Contenido")]
+        [Required(ErrorMessage = "Debe escribir un comentario")]
+        [DisplayFormat(DataFormatString = "{0:dd-mm-yyyy hh:mm}", ApplyFormatInEditMode = true)]
+        public DateTime Hora { get; set; }
+        
+        
     }
 }
