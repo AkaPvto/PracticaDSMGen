f12b3fa57f1b41e2ac9e6136cc1a8493e0c88cd3
SergioMiedes <smg163@gcloud.ua.es>
2022-06-26 19:34:11 +0200

    Carpeta Assemblers

---
+++ GoGaming/Models/ComentarioViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoGaming.Models
+{
+    public class ComentarioViewModel
+    {
+        public int Id { get; set; }
+    }
+}
