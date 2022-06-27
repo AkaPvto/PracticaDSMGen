1a1b1f3ca565035efbd7a6b6343bd3b5146283ab
AkaPvto <jrv37@gcloud.ua.es>
2022-06-27 13:16:32 +0200

    Creando MVC de juego, a medias de momento

---
+++ GoGaming/Models/JuegoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GoGaming.Models
+{
+    public class JuegoViewModel
+    {
+        [Display(Prompt = "Nombre", Description = "Nombre del juego", Name = "Nombre")]
+        [Required(ErrorMessage = "Debe escribir el nombre del juego")]
+        [StringLength(maximumLength: 200, ErrorMessage = "El nombre del juego no puede tener mas de 200 caracteres")]
+        public string Nombre { get; set; }
+
+        [Display(Prompt = "Descripcion", Description = "Sinopsis del juego", Name = "Descripcion")]
+        [Required(ErrorMessage = "Debes escribir una descripcion del juego")]
+        [StringLength(maximumLength: 500, ErrorMessage = "La descripcion no puede tener mas de 500 caracteres")]
+        public string Descripcion { get; set; }
+
+        [Display(Prompt = "Portada", Description = "Imagen correspondiente al juego", Name = "Portada")]
+        [Required(ErrorMessage = "Debes indicar una imagen para el juego")]
+        [StringLength(maximumLength: 100, ErrorMessage = "El nombre de la imagen no puede tener mas de 100 caracteres")]
+        public string Portada { get; set; }
+    }
+}
