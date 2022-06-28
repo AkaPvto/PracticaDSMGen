d6c5780e7b3465d1313ccb07b7c4dcd699f87a1f
SergioMiedes <smg163@gcloud.ua.es>
2022-06-28 20:39:49 +0200

    Toda la parte básica del comentario hecha

--- GoGaming/Models/ComentarioViewModel.cs
+++ GoGaming/Models/ComentarioViewModel.cs
@@ -17,8 +17,8 @@ namespace GoGaming.Models
         [StringLength(maximumLength: 400, ErrorMessage = "El comentario no puede tener más de 400 caracteres")]
         public string Contenido { get; set; }
 
-        [Display(Prompt = "Comentario", Description = "Comentario", Name = "Contenido")]
-        [Required(ErrorMessage = "Debe escribir un comentario")]
+        [Display(Prompt = "Fecha y hora", Description = "Fecha y hora", Name = "Hora")]
+        [Required(ErrorMessage = "Debe escribir una fecha")]
         [DisplayFormat(DataFormatString = "{0:dd-mm-yyyy hh:mm}", ApplyFormatInEditMode = true)]
         public DateTime Hora { get; set; }
         
