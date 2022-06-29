9e0dc51bcc11a044b08081302b7bc822c7848bcb
SaraMorote <92912873+SaraMorote@users.noreply.github.com>
2022-06-29 14:30:18 +0200

    cambios

--- GoGaming/Controllers/HomeController.cs
+++ GoGaming/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using GoGaming.Assemblers;
+using GoGaming.Models;
+using PracticaDSMGenNHibernate.CAD.DSMPracticas;
+using PracticaDSMGenNHibernate.CEN.DSMPracticas;
+using PracticaDSMGenNHibernate.EN.DSMPracticas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,23 +11,51 @@ using System.Web.Mvc;
 
 namespace GoGaming.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BasicController
     {
         public ActionResult Index()
         {
+            /*SessionInitialize();
+
+            JuegoCAD juCAD = new JuegoCAD(session);
+            JuegoCEN juCEN = new JuegoCEN(juCAD);
+
+            IList<JuegoEN> listEN = juCEN.ReadAll(0, -1);
+            IList<JuegoEN> revList = new List<JuegoEN>();
+            for (int i = listEN.Count() - 1; i >= 0; i--)
+            {
+                revList.Add(listEN.ElementAt(i));
+                if (i < listEN.Count() - 9)
+                {
+                    break;
+                }
+            }
+            ViewData["JuegoReci"] = listEN;
+            ViewData["iter"] = 0;
+
+            ComunidadCAD comCad = new ComunidadCAD(session);
+            ComunidadCEN comCEN = new ComunidadCEN(comCad);
+            IList<ComunidadEN> lista = comCEN.ReadAll(0, 5);
+
+            IEnumerable<ComunidadViewModel> listViewModel = (IEnumerable<ComunidadViewModel>)new ComunidadAssembler().ConvertListENTModel(lista).ToList();
+
+            SessionClose();
+
+            return View(listViewModel);*/
+
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Sobre Go-Gaming";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Nuestra página de contacto";
 
             return View();
         }
